d6defb921a9726ce966c160a046802ee9a1e5124
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-03-25 11:18:49 +0100

    manipulate timezone data according to language parameter

---
+++ data/real/index.js
@@ -0,0 +1,41 @@
+const Africa = require("./Africa");
+
+const tzlistRaw = [...Africa];
+
+const tzlist = (lang) => {
+  let language;
+  switch (lang) {
+    case "de":
+    case "en":
+    case "fr":
+      language = lang;
+      break;
+    default:
+      language = "en";
+  }
+  return tzlistRaw.map((country) => {
+    country.country = country.country[language];
+    if (country.subdiv) {
+      country.subdiv = country.subdiv.map((subdivision) => {
+        const { title, extra = undefined } = subdivision;
+        const newDiv = {};
+        if (typeof title === "string") {
+          newDiv.title = title;
+        } else {
+          newDiv.title = title[language];
+        }
+        if (extra) {
+          if (typeof extra === "string") {
+            newDiv.extra = extra;
+          } else {
+            newDiv.extra = extra[language];
+          }
+        }
+        return newDiv;
+      });
+    }
+    return country;
+  });
+};
+
+module.exports = tzlist;
