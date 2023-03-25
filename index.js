06d71d27498748b73e505b26fa84c2a96998d863
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-03-25 11:19:02 +0100

    new real endpoint

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const port = 3600;
+
+const real = require("./data/real/index");
+
+function corsOptions(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "application/json");
+  next();
+}
+
+app.use("*", (req, res, next) => corsOptions(req, res, next));
+
+app.get("/real", (req, res) => {
+  const { lang = "en" } = req.query;
+  return res.status(200).json(real(lang));
+});
+
+app.all("*", (req, res) => {
+  return res
+    .status(400)
+    .json({ error: { message: "No valid endpoint matched." } });
+});
+
+app.listen(port, () => {
+  console.log(`Listening for requests on port ${port} ...`);
+});
